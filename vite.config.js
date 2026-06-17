4f872d0cc8d0feeea466316385771aeb5707c889
Prathamesh <prathamesh.patil@infraplan.co.in>
2026-06-17 17:41:47 +0530

    fix: remove tailwind config

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// 1. Import the visualizer at the top
 
 export default defineConfig({
-  plugins: [
-    react(), 
-   
-  ],
+  plugins: [react()],
 })
-
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-// import tailwindcss from "@tailwindcss/vite";
-
-// export default defineConfig({
-//   plugins: [react(), tailwindcss()],
-//   build: {
-//     rollupOptions: {
-//       output: {
-//         manualChunks(id) {
-//           if (id.includes("node_modules")) {
-//             return "vendor";
-//           }
-//         }
-//       }
-//     }
-//   }
-// });
