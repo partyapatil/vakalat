fae7854b9aa58bc45d94afad6341413ee8b24370
Prathamesh <prathamesh.patil@infraplan.co.in>
2026-06-17 17:49:34 +0530

    added only

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,14 @@
-import { Routes, Route, Link } from "react-router-dom";// import Home from "./pages/Home";
-import Movies from "./pages/Movies";
-import MovieDetails from "./pages/MovieDetails";
-import Home from "./pages/Home";
-import React, { Suspense } from "react";
+// App.jsx
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 import VakalatnamaEditor from "./pages/V";
 
-// import Movies from "./pages/Movies";
-// import MovieDetails from "./pages/MovieDetails";
-// import NotFound from "./pages/NotFound";
-const Testing=React.lazy(() => import("./pages/Testing"));
 export default function App() {
   return (
     <div>
-      <Suspense fallback="Loading...">
-
-     
-      <nav style={{ display: "flex", gap: "16px", padding: "16px", backgroundColor: "#f0f0f0" }}>
-        <Link to="/">Home</Link>
-        <Link to="/movies">Movies</Link>
-        <Link to="/test">Testing</Link>
-      </nav>
-
       <Routes>
-        {/* <Route path="/" element={<Home />} /> */}
-        <Route path="/movies" element={<Movies />} />
-        <Route path="/test" element={<Testing />} />
-        <Route path="/vakalt" element={<VakalatnamaEditor />} />
-
-        <Route path="/" element={<Home />} />
-        <Route path="/movies/:id" element={<MovieDetails />} />
-        {/* <Route path="/movies/:id" element={<MovieDetails />} /> */}
-        {/* <Route path="*" element={<NotFound />} /> */}
+        <Route path="/" element={<VakalatnamaEditor />} />
       </Routes>
-       </Suspense>
     </div>
   );
 }
