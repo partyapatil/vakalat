6ff2ba89f02533f37d63a19aa8000def3614f45c
Prathamesh <prathamesh.patil@infraplan.co.in>
2026-06-17 17:36:05 +0530

    fix: remove unused tailwind config

--- src/pages/practice/PromiseAll.jsx
+++
@@ -1,8 +0,0 @@
-const PromiseAllpage = (getall) => {
-    return new Promise((res, rej) => {
-
-    Promise.all(getall).then(e=>console.log(resolved))
-  });
-};
-
-export default PromiseAllpage;
--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 // 1. Import the visualizer at the top
-import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
   plugins: [
     react(), 
-    tailwindcss(),
-    // 2. Add it here to the plugins array
-    visualizer({
-      open: true, // This will automatically open the stats chart in your browser when the build finishes
-    })
+   
   ],
 })
 
